36408857c989fe54a3f88fb3caa62c08d8c60f83
George de Pinho <georgedipinho@hotmail.com>
2022-05-29 15:19:45 -0300

    relacionamentos

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -4,10 +4,17 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,19 +24,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler" })
 public class Aluno {
+	
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String nome;
 
+  @Column(unique = true)
   private String cpf;
 
   private String bairro;
 
   private LocalDate dataDeNascimento;
-
+  
+  
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY) // quando tiver requisicao, vai carregar todas as informacões MENOS a lista de avaliacao fisica
+  @JsonIgnore
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
