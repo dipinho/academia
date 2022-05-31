46314de5e003100f3c2ffad90de79e03f66c751f
George de Pinho <georgedipinho@hotmail.com>
2022-05-30 23:45:50 -0300

    Avaliacao fisica ok

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -1,7 +1,12 @@
 package me.dio.academia.digital.entity.form;
 
-import me.dio.academia.digital.entity.Aluno;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvaliacaoFisicaForm {
 
   private Long alunoId;
@@ -9,4 +14,29 @@ public class AvaliacaoFisicaForm {
   private double peso;
 
   private double altura;
+
+public Long getAlunoId() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public double getPeso() {
+	return peso;
+}
+
+public void setPeso(double peso) {
+	this.peso = peso;
+}
+
+public double getAltura() {
+	return altura;
+}
+
+public void setAltura(double altura) {
+	this.altura = altura;
+}
+
+public void setAlunoId(Long alunoId) {
+	this.alunoId = alunoId;
+}
 }
